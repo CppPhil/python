f51736fc472b551269cc57fadaa090b7d56e736f
CppPhil <CppPhil@users.noreply.github.com>
2020-07-04 21:46:56 +0200

    Update

--- histogram/main.py
+++ histogram/main.py
@@ -1,3 +1,7 @@
+from __future__ import division
+
+import math
+
 import matplotlib.pyplot as plt
 from collections import Counter
 
@@ -12,3 +16,86 @@ plt.title("Histogram of Friend Counts")
 plt.xlabel("# of friends")
 plt.ylabel("# of people")
 plt.show()
+
+num_points = len(num_friends)
+
+largest_value = max(num_friends)
+smallest_value = min(num_friends)
+
+sorted_values = sorted(num_friends)
+second_smallest_value = sorted_values[1]
+second_largest_value = sorted_values[-2]
+
+
+def mean(x):
+    return sum(x) / len(x)
+
+
+print(mean(num_friends))
+
+
+def median(v):
+    """finds the 'middle-most' value of v"""
+    n = len(v)
+    sorted_v = sorted(v)
+    midpoint = n // 2
+
+    if n % 2 == 1:
+        # if odd, return middle value
+        return sorted_v[midpoint]
+    else:
+        # if even, return the average of the middle values
+        lo = midpoint - 1
+        hi = midpoint
+        return (sorted_v[lo] + sorted_v[hi]) / 2
+
+
+print(median(num_friends))
+
+
+def quantile(x, p):
+    """returns the pth-percentile value in x"""
+    p_index = int(p * len(x))
+    return sorted(x)[p_index]
+
+
+def mode(x):
+    """returns a list, might be more than one mode"""
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.iteritems() if count == max_count]
+
+
+def data_range(x):
+    return max(x) - min(x)
+
+
+def de_mean(x):
+    """translate x by subtracting its mean (so the result has mean 0)"""
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+
+def dot(v, w):
+    """v_1 * w_1 + ... v_n * w_n"""
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v):
+    """v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
+
+
+def variance(x):
+    """assumes x has at least two elements"""
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n - 1)
+
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+
+def interquartile_range(x):
+    return quantile(x, 0.75) - quantile(x, 0.25)
